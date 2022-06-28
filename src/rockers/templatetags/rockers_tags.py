82d34b7bee3c68983fe53cb867adfbbb41572983
oznob5 <geadamia@gmail.com>
2022-06-28 17:19:53 +0300

    added another template tag

--- src/rockers/templatetags/rockers_tags.py
+++ src/rockers/templatetags/rockers_tags.py
@@ -11,3 +11,14 @@ def show_roles(sort=None, role_selected=0):
         roles = Role.objects.order_by(sort)
 
     return {"roles": roles, "role_selected": role_selected}
+
+@register.inclusion_tag('rockers/tags/list_menu.html')
+def show_menu():
+
+    menu = [
+        {'title': 'About', 'url_name': 'about'},
+        {'title': 'Add an article', 'url_name': 'add_article'},
+        {'title': 'Feedback', 'url_name': 'feedback'},
+    ]
+
+    return {"menu": menu}
